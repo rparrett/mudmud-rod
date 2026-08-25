5f9e33e72d17fdf102c0b27705c3d68be54a3848
Rob Parrett <robparrett@gmail.com>
2026-08-25 15:45:07 -0700

    Remove dangerous default from scanwalk automation

--- automation/scanwalk.lua
+++ automation/scanwalk.lua
@@ -1,7 +1,16 @@
-local route = matches[2] or "s,s,s,nw"
-local commands = rod.parse_dirs(route)
+local route_spec = matches[2]
+if not route_spec then
+    rod.echoln({
+        { text = "Usage: ", foreground = ansi.bright_red },
+        "scanwalk <direction spec or route name>",
+    })
+    return
+end
 
-seq.start("test scan route", function(path)
+local commands = rod.parse_dirs(route_spec)
+local sequence_name = rod.dirs[route_spec] and "scanwalk " .. route_spec or "scanwalk"
+
+seq.start(sequence_name, function(path)
     for _, command in ipairs(commands) do
         if rod.is_direction(command) then
             rod.move(path, command, 5)
